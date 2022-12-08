ab15b6ebf7fd10cd2f81351c10eb01853c5c43b6
KyleFC <Kwatonna@gmail.com>
2022-12-08 15:40:46 -0800

    Bozo

---
+++ rizzgod.py
@@ -0,0 +1,13 @@
+from dialogue import Dialogue
+class Player:
+    def __init__(self):
+        self.rizz = 0
+        self.money = 0
+        self.maidens = 0
+        self.dates = 0
+        self.action_controller = Dialogue
+        pass
+    
+
+if __name__ == "__main__":
+    pass
