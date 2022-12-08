cadb7375a9bebd2f4e6afa0a280b8122ed1b124c
Robby La Tourette <56559210+robby-latourette@users.noreply.github.com>
2022-12-08 10:03:35 -0800

    AOOOOGA

--- action_pool.py
+++ action_pool.py
@@ -1,18 +1,18 @@
 actions = {}
 #actions are tuples. ('string action', base chance, personality)
-actions['rizz'] = [('You hit them with a lightskin stare.', 30), ('You present them with your fortnite moves.', 20), ('You tell them the funniest joke they have ever heard.', 60), 
-('You guessed their favorite drink.', 70), ('You attempt to hold hands.', 30), 
-('You give them a nice compliment.', 50), ('You bite your lips and look them up and down.', 40)]
-actions['money'] = [('You "accidently" dropped your fat wallet.', 60), ('You offer to pay for any meal they want.', 60), 
-('You offer a car ride in your bugatti', 70), ('You offer to pay off their college debt.', 80)]
-actions['strength'] = [('You pick a fight with the bouncer.', 20), ('You start a brawl among everyone else in the room.', 40), 
-('You demonstrate value by opening a pickle jar.', 50), ('You drop down and give her 50 pushups.', 60), 
-('You accidently flex to hard and rip your shirt.', 60), ('You save a guy from getting bullied.', 50), 
-('You show her a video of you wrestling a bear.', 60)]
-actions['other'] = [('You tell them you play a Riot game.', 10), ('You show them your instagram meme page.', 10),
-('You tell them you watch anime', 10), ('You use a jedi mind trick to convince her you dont look like a goblin', 100), 
-('You recite the periodic table from memory', 15), ('"...but he made graduation."', 50), 
-('You show them your hotwheel collection.', 5)]
-actions['buzzerbeater'] = [('You grab their face and kiss them.', 40), ('You ask them to clarify whether theyre a girl or guy', 10), 
-('You get a little tipsy and expose government secrets.', 40), ('You tell them youre a computer science major', 6), 
-('You steal some money from her purse. Times are rough.', 10), ('You propose.', 15)]
+actions['rizz'] = [('You hit them with a lightskin stare.', 30, 5), ('You present them with your fortnite moves.', 20, 7), ('You tell them the funniest joke they have ever heard.', 60, 10), 
+('You guessed their favorite drink.', 70,4), ('You attempt to hold hands.', 30,9), 
+('You give them a nice compliment.', 50, 6), ('You bite your lips and look them up and down.', 40, 5)]
+actions['money'] = [('You "accidently" dropped your fat wallet.', 60,7), ('You offer to pay for any meal they want.', 60,9), 
+('You offer a car ride in your bugatti', 70), ('You offer to pay off their college debt.', 80, 15)]
+actions['strength'] = [('You pick a fight with the bouncer.', 20, 6), ('You start a brawl among everyone else in the room.', 40,4), 
+('You demonstrate value by opening a pickle jar.', 50, 80), ('You drop down and give her 50 pushups.', 60, 3), 
+('You accidently flex to hard and rip your shirt.', 60, 8), ('You save a guy from getting bullied.', 50, 7), 
+('You show her a video of you wrestling a bear.', 60, 10)]
+actions['other'] = [('You tell them you play a Riot game.', 10, 16), ('You show them your instagram meme page.', 10, 0),
+('You tell them you watch anime', 10), ('You use a jedi mind trick to convince her you dont look like a goblin', 100, 15), 
+('You recite the periodic table from memory', 15, 1), ('"...but he made graduation."', 50, 14), 
+('You show them your hotwheel collection.', 5, 10)]
+actions['buzzerbeater'] = [('You grab their face and kiss them.', 12, 60), ('You ask them to clarify whether theyre a girl or guy', 10, 20), 
+('You get a little tipsy and expose government secrets.', 40, 10), ('You tell them youre a computer science major', 6, 15), 
+('You steal some money from her purse. Times are rough.', 10, 0), ('You propose.', 9, 100), ('You stare at her toes while yelling, "AOOOOOOOGA AOOOOOOOGA"', 15, 20)]
