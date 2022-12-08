af112358798e15c2266938a5e67833750b8e2f3b
Robby La Tourette <56559210+robby-latourette@users.noreply.github.com>
2022-12-08 07:19:46 -0800

    edited personality

--- Character.py
+++ Character.py
@@ -1,2 +1,4 @@
+import Personality
+
 class Character:
-    pass
+    temp = Personality
---
+++ Personality.py
@@ -0,0 +1,10 @@
+import random
+class Personality:
+    traitList = {'sporty':True} 
+
+    def getTrait():
+        pass
+
+if __name__ == "__main__":
+    per = Personality
+    print(per.traitList['sporty'])
