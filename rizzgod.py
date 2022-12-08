c22c44cd13d9ab63ad0ce72090f03fddccf5b2c5
KyleFC <Kwatonna@gmail.com>
2022-12-08 15:58:21 -0800

    added action function

--- rizzgod.py
+++ rizzgod.py
@@ -1,12 +1,22 @@
+import random
 from dialogue import Dialogue
 class Player:
     def __init__(self):
-        self.rizz = 0
-        self.money = 0
-        self.maidens = 0
-        self.dates = 0
+        self.stat_dict = {"rizz": 0, "money": 0, "dates": 0, "age": 20, "strength": 0}
+
         self.action_controller = Dialogue
-        pass
+
+    def perform_action(self, action):
+        r_num = random.random(0, 1)
+        success_rate = self.stat_dict[action[1]] + action[2]
+        if success_rate > r_num:
+            print(f"{action[1].upper()} INCREASED BY 1 POINT!")
+        else:
+            print("You were unsuccessful :(")
+    
+            
+
+
     
 
 if __name__ == "__main__":
