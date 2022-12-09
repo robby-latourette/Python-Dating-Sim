d8a119a7c58e50f0635ae3bbc09cafbe143fef1a
KyleFC <Kwatonna@gmail.com>
2022-12-08 17:53:29 -0800

    all actions

--- action_pool.py
+++ action_pool.py
@@ -9,5 +9,10 @@ actions['strength'] = [('You pick a fight with the bouncer.', 20), ('You start a
 ('You demonstrate value by opening a pickle jar.', 50), ('You drop down and give her 50 pushups.', 60), 
 ('You accidently flex to hard and rip your shirt.', 60), ('You save a guy from getting bullied.', 50), 
 ('You show her a video of you wrestling a bear.', 60)]
-actions['other'] = ''
-actions['buzzerbeater'] = ''
+actions['other'] = [('You tell them you play a Riot game.', 10), ('You show them your instagram meme page.', 10),
+('You tell them you watch anime', 10), ('You use a jedi mind trick to convince her you dont look like a goblin', 100), 
+('You recite the periodic table from memory', 15), ('"...but he made graduation."', 50), 
+('You show them your hotwheel collection.', 5)]
+actions['buzzerbeater'] = [('You grab their face and kiss them.', 40), ('You ask them to clarify whether theyre a girl or guy', 10), 
+('You get a little tipsy and expose government secrets.', 40), ('You tell them youre a computer science major', 6), 
+('You steal some money from her purse. Times are rough.', 10), ('You propose.', 15)]
