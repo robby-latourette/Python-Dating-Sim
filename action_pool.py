fe91c309dc8ec55521c300282f5a10e963bd662b
KyleFC <Kwatonna@gmail.com>
2022-12-08 17:40:23 -0800

    working on actions

---
+++ action_pool.py
@@ -0,0 +1,13 @@
+actions = {}
+#actions are tuples. ('string action', base chance, personality)
+actions['rizz'] = [('You hit them with a lightskin stare.', 30), ('You present them with your fortnite moves.', 20), ('You tell them the funniest joke they have ever heard.', 60), 
+('You guessed their favorite drink.', 70), ('You attempt to hold hands.', 30), 
+('You give them a nice compliment.', 50), ('You bite your lips and look them up and down.', 40)]
+actions['money'] = [('You "accidently" dropped your fat wallet.', 60), ('You offer to pay for any meal they want.', 60), 
+('You offer a car ride in your bugatti', 70), ('You offer to pay off their college debt.', 80)]
+actions['strength'] = [('You pick a fight with the bouncer.', 20), ('You start a brawl among everyone else in the room.', 40), 
+('You demonstrate value by opening a pickle jar.', 50), ('You drop down and give her 50 pushups.', 60), 
+('You accidently flex to hard and rip your shirt.', 60), ('You save a guy from getting bullied.', 50), 
+('You show her a video of you wrestling a bear.', 60)]
+actions['other'] = ''
+actions['buzzerbeater'] = ''
--- dialogue.py
+++ dialogue.py
@@ -1,10 +1,10 @@
 class Dialogue:
 
     def __init__(self):
-        self.actions = []
-        self.question_pool = []
+        self.actions = ['rizz', 'money', 'strength', 'other']
+        self.question_pool = 
         self.depth = 0
-        
+
     def give_actions(self, time):
         if time < 5:
             return "Buzzer Beater"
